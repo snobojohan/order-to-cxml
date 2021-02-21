714dbfd103255ca5fa31d1dc28b1353c41358d4c
Johan Hallberg <johan.hallberg@gmail.com>
2021-02-21 23:40:43 +0100

    file

---
+++ order-to-cxml.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Plugin Name: Order to cxml
+* Plugin URI: https://bandalsim.org/
+* Description: This is the very first version.
+* Version: 1.0
+* Author: Johan Hallberg
+* Author URI: http://bandalism.org/
+**/
+
+?>
